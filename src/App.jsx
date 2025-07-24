e016ca89bc5485df103bb02224fbea875aeec5e5
Andrew White <andrewwhite@Andrews-MBP.search.charter.net>
2025-07-23 19:02:48 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ArtistProfile from "./pages/ArtistProfile";
+
+export default function App() {
+  return <ArtistProfile />;
+}
---
+++ src/pages/ArtistProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function ArtistProfile() {
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      color: 'lime',
+      fontSize: '48px',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      ✅ ArtistProfile Component Loaded
+    </div>
+  );
+}
